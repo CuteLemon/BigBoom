3e28de24f062740fccc480a07a4b59386817618f
CuteLemon <lemontechmaster@gmail.com>
2019-09-12 11:18:03 +0800

    use apple stock price, send it to kafka

--- real-time-stock-trader/stock_price_receiver/data_monitor.py
+++ real-time-stock-trader/stock_price_receiver/data_monitor.py
@@ -7,11 +7,14 @@ from kafka import KafkaProducer
 import time
 import json
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                        value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 data = pd.read_csv('aapl-trading-hour.csv')
+data.rename(columns={data.columns[0]:'timestamp'},inplace=True)
+data.set_index('timestamp',inplace=True)
 
 if __name__ == "__main__":
     while True:
         for k,v in data.iterrows():
-            producer.send('test',key=k,value=v)
+            producer.send('test',{k:v.to_json()})
