b9cb0719a252b25e8d4aca10d8194d97ecf2be4f
CuteLemon <lemontechmaster@gmail.com>
2019-11-20 16:38:04 +0800

    feat: added start data_monitor & spark_script

--- real-time-stock-trader/start.sh
+++ real-time-stock-trader/start.sh
@@ -7,3 +7,14 @@ cd ..
 cd ..
 cd ./spark/docker-spark
 docker-compose up -d
+
+# 数据发送模拟程序
+cd ..
+cd ..
+cd ./real-time-stock-trader
+cd ./stock_price_receiver/
+python data_monitor.py &
+
+# spark 处理程序
+cd ../data_buffer/
+python model_v1.py
