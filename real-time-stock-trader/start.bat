200d6109f05da69405b5e7781494daac8344d2e2
Zhipeng <song_zhipeng@foxmail.com>
2019-11-20 20:53:27 +0800

    feat: add windows bat script to start monitor&spark_script

--- real-time-stock-trader/start.bat
+++ real-time-stock-trader/start.bat
@@ -5,3 +5,13 @@ cd ..
 cd ..
 cd ./spark/docker-spark
 docker-compose up -d
+
+cd ..
+cd ..
+cd ./real-time-stock-trader
+cd ./stock_price_receiver/
+python data_monitor.py &
+
+: spark 处理程序
+cd ../data_buffer/
+python model_v1.py
