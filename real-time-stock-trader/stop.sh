836423fe2be522afc0d06e24828d13ebcf48ec8f
CuteLemon <lemontechmaster@gmail.com>
2019-11-20 16:39:03 +0800

    feat: a script to stop spark&kafka docker cluster

---
+++ real-time-stock-trader/stop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# 停止kafka&spark容器集群
+docker ps | grep -E 'kafka|spark' | awk '{print $1}' |xargs docker stop
+
+# 停止数据发送模拟程序
+ps | grep data_monitor|awk '{print $1}'| xargs kill
